a6141cc2ba3ea091f8bc9989869745a857d0e8dc
18322693247@163.com <18322693247@163.com>
2020-01-09 21:10:18 +0800

    start a algorithm practices project

---
+++ Permutations/index.js
@@ -0,0 +1,37 @@
+function permutations(str) {
+  let begin = 0,
+    result = [];
+  permutation(str.split(''), begin, result);
+  return result;
+}
+
+function permutation(str, begin, result) {
+  if (begin + 1 === str.length) {
+    result.push(str.join(""));
+  } else {
+    for (let i = begin; i < str.length; i++) {
+      if (notSame(str, begin, i)) {
+        swap(str, begin, i);
+        permutation(str, begin + 1, result);
+        swap(str, begin, i)
+      }
+    }
+  }
+}
+
+function notSame(str, begin, end) {
+  for (let p = begin; p < end; p++) {
+    if (str[p] === str[end]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function swap(str, a, b) {
+  let temp = str[a];
+  str[a] = str[b];
+  str[b] = temp;
+}
+
+console.log(permutations("aabb"));
