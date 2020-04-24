d1f7192b5a7b82b8c6ffc41dcae0d1c7cf7464cc
18322693247@163.com <18322693247@163.com>
2020-04-24 18:49:02 +0800

    提交题目

---
+++ ConwayGame/index.js
@@ -0,0 +1,21 @@
+/**
+ * 细胞游戏，代数变化
+ * @param{Array<Array<number>>} cells 细胞图
+ * @param{number} generations 代数
+ * @return{Array<Array<number>>} 第几代细胞图
+ */
+function getGeneration(cells, generations) {
+  let result = []
+  // 循环代数
+  for (let g = 0; g < generations; g++) {
+    // 纵坐标
+    for (let i = 0; i < cells.length; i++) {
+      // 横坐标
+      for (let j = 0; j < cells[i].length; j++) {
+        
+      }
+    }
+  }
+
+  return cells
+}
