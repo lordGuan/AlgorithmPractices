34b3fe3e7b7d8c71c599eb0447af1173133227fa
usw <guanguolimail@gmail.com>
2020-09-30 12:06:13 +0800

    解题提交

--- MajorityElement/index.ts
+++ MajorityElement/index.ts
@@ -1,12 +1,47 @@
+/**
+ * 先排序，再测试
+ * 但是会产生副作用
+ * 且不知道为什么内存占用奇高，可能是排序过程内存消耗
+ * @param nums
+ */
 export function majorityElement(nums: number[]): number {
     if (nums.length <= 0) return -1
     if (nums.length === 1) return nums[0]
-    const s = nums.sort((a, b) => a - b)
-    const l = s.length
-    const middle = Math.ceil(l / 2 - 1)
+    nums.sort((a, b) => a - b)
+    const middle = Math.ceil(nums.length / 2 - 1)
     for (let i = 0; i <= middle; i++) {
-        let last = l % 2 === 0 ? 0 : 1
-        if (s[i] === s[i + middle - last]) return s[i]
+        if (nums[i] === nums[i + middle]) return nums[i]
+    }
+    return -1
+}
+
+/**
+ * 摩尔投票法
+ * 执行速度和内存占用不相上下，内存不知道消耗在哪了
+ * 用Map的方式居然是最省内存
+ * @param nums
+ */
+export function majorityElement_2(nums: number[]): number {
+    if (nums.length <= 0) return -1
+    let count = 0
+    let major = nums[0]
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            major = nums[i]
+            count++
+        } else {
+            nums[i] === major && count++ || count--
+        }
+    }
+    if (count > 0) {
+        let check = 0
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === major) {
+                check++
+                if (check > nums.length / 2) return major
+            }
+        }
+        return -1
     }
     return -1
 }
--- MajorityElement/test.ts
+++ MajorityElement/test.ts
@@ -1,6 +1,14 @@
-import {majorityElement} from "./index";
+import {majorityElement, majorityElement_2} from "./index";
 
 test('majorityElement', () => {
-    expect(majorityElement([3,2,3])).toBe(3)
-    expect(majorityElement([-1,1,1,1,2,1])).toBe(1)
+    expect(majorityElement([3, 2, 3])).toBe(3)
+    expect(majorityElement([-1, 1, 1, 1, 2, 1])).toBe(1)
+})
+
+
+test('majorityElement', () => {
+    expect(majorityElement_2([3, 2, 3])).toBe(3)
+    expect(majorityElement_2([-1, 1, 1, 1, 2, 1])).toBe(1)
+    expect(majorityElement_2([3,2,1])).toBe(-1)
+
 })
